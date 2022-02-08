6dc08bd04ec0ee44a5374305680820299014922d
Eamon Sheil <Eamondsheil@gmail.com>
2022-02-08 13:08:34 -0500

    added navbar

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by Javascript";
-document.querySelector("body").appendChild(h2);
+//const h2 = document.createElement("h2");
+//h2.textContent = "This content added by Javascript";
+//document.querySelector("body").appendChild(h2);
+
+//console.log(1 + 2 + "puppies" + 4 + 5);
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+    console.log('I was called!');
+  }
